60d157f9784992379b1baef2950869c7c6b97304
imanujagarwal <anuj339@gmail.com>
2017-10-15 15:07:53 +0530

    int

---
+++ app/src/main/java/com/google/firebase/wittychat/MessageAdapter.java
@@ -0,0 +1,35 @@
+package com.google.firebase.wittychat;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+
+import java.util.List;
+
+public class MessageAdapter extends ArrayAdapter<FriendlyMessage> {
+    public MessageAdapter(Context context, int resource, List<FriendlyMessage> objects) {
+        super(context, resource, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = ((Activity) getContext()).getLayoutInflater().inflate(R.layout.item_message, parent, false);
+        }
+
+        TextView messageTextView = convertView.findViewById(R.id.messageTextView);
+        TextView authorTextView =  convertView.findViewById(R.id.nameTextView);
+
+        FriendlyMessage message = getItem(position);
+
+        messageTextView.setText(message.getText());
+
+        authorTextView.setText(message.getName());
+
+        return convertView;
+    }
+}
